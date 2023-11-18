798a4a73718f9e4ec2917ab30370c94b832f52ae
Yasmim Rosa <mimsayrosa@gmail.com>
2023-11-18 18:20:38 -0300

    criando tabela

---
+++ backend/controllers/ElectiveControllers.js
@@ -0,0 +1,5 @@
+const { Electives } = require('../models/schemas/Electives')
+
+exports.createElective = async(req, res) => {
+    // const { }
+}
---
+++ backend/views/routes/Electives.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const router = express.Router();
+const userController =  require('../../controllers/ElectiveControllers')
+
+// router.post("/", userController.registerUser);
+// router.post("/teste", userController.teste);
+
+module.exports = router; 
