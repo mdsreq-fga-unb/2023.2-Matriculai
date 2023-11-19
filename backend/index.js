fad775624117495dedfcba3ac135e196b83c3df8
Yasmim Rosa <mimsayrosa@gmail.com>
2023-11-19 19:21:55 -0300

    atualização rota de usuários

--- backend/index.js
+++ backend/index.js
@@ -11,7 +11,7 @@ const port = 3001;
 app.use(express.json());
 app.use(validateToken);
 
-app.use('/', userRoute);
+app.use(userRoute);
 app.use('/elective', electiveRoute);
 app.use('/learning_paths', learningPathRoute);
 
