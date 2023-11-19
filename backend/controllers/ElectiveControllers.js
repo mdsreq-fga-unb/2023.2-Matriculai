a898264782a3f724c094b0edf7f82f9adb911937
Yasmim Rosa <mimsayrosa@gmail.com>
2023-11-19 18:01:53 -0300

    método deleteElective

--- backend/controllers/ElectiveControllers.js
+++ backend/controllers/ElectiveControllers.js
@@ -4,7 +4,7 @@ const { Electives } = require('../models/schemas')
 exports.createElective = async(req, res) => {
     const { name, description, school_year, teacher, vacancies, schedules } = req.body
 
-    Electives.create({
+    await Electives.create({
         name: name,
         description: description,
         school_year: school_year,
@@ -13,6 +13,17 @@ exports.createElective = async(req, res) => {
         schedules: schedules
     }).then(() => {
         res.status(201).json("OK")
+    }).catch((err) => {
+        if(err){
+            res.status(400).json({error: err})
+        }
+    })
+}
+
+exports.deleteElective = async(req, res) => {
+    const { id }  = req.body
+    await Electives.destroy({ where: {id: id}}).then(() => {
+            res.status(200).json("OK")
     }).catch((err) => {
         if(err){
             res.status(400).json({error: err})
--- backend/views/routes/Electives.js
+++ backend/views/routes/Electives.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const electivesController =  require('../../controllers/ElectiveControllers')
 
 router.post("/elective/createElective", electivesController.createElective);
+router.delete("/elective/", electivesController.deleteElective)
 
 module.exports = router; 
