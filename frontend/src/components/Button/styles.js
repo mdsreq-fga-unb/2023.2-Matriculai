674194922759a13fd6ba5a812f655125522814f6
Algusto-RC <algustorc@gmail.com>
2023-11-20 09:35:15 -0300

    test 5

---
+++ frontend/src/components/Button/styles.js
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+export const Button = styled.button`
+  padding: 16px 20px;
+  outline: none;
+  border: none;
+  border-radius: 5px;
+  width: 100%;
+  cursor: pointer;
+  background-color: #243A69;
+  border-radius: 30px;
+  color: white;
+  font-weight: 600;
+  font-size: 16px;
+  max-width: 350px;
+  justify-self: center;
+`;
