5005e5819a4155341d1e97c69fcba90bdad20fdf
Algusto-RC <algustorc@gmail.com>
2023-11-20 23:30:21 -0300

    primeira versão do header e logout
    Co-authored-by: luanasoares0901<luana.soares0901@gmail.com>

--- frontend/src/pages/Home/index.js
+++ frontend/src/pages/Home/index.js
@@ -1,21 +1,48 @@
 import React from "react";
-import { useNavigate } from "react-router-dom";
-import Button from "../../components/Button";
-import useAuth from "../../hooks/useAuth";
-import * as C from "./styles";
+import { Link } from "react-router-dom";
+import { Flex, Box, Heading, Spacer, Text, Center, Menu, MenuButton, MenuList, MenuItem, Button} from "@chakra-ui/react"; // Importe os componentes do Chakra UI ou outro framework que você esteja usando
+import { Image } from '@chakra-ui/react'
+import cmtnLogo from '../../img/cmtnLogo.png'
+import menuHamburguer from '../../icon/menuHamburguer.png'
 
-const Home = () => {
-  const { signout } = useAuth();
-  const navigate = useNavigate();
-
+const Header = () => {
   return (
-    <C.Container>
-      <C.Title>Home</C.Title>
-      <Button Text="Sair" onClick={() => [signout(), navigate("/")]}>
-        Sair
-      </Button>
-    </C.Container>
+    <Flex
+      as="header"
+      align="center"
+      justify="space-between"
+      padding="1rem"
+      backgroundColor="#243A69"
+      color="white"
+    >
+      <Box>
+        <Heading as="h1" size="lg">
+          <Box maxWidth={'120px'} maxHeight={'120px'}  paddingLeft='30px'>
+            <Image src={cmtnLogo} alt='student' style={{ maxWidth: '100%', height: 'auto' }} />
+          </Box>
+        </Heading>
+      </Box>
+      <Spacer />
+      <Box>
+        <Link to="/">Home</Link>
+          <Box  maxWidth={'40px'} maxHeight={'40px'} marginTop={'-12px'} marginBottom={'3px'} >
+          <Menu>
+            <MenuButton border={'none'} bgColor={'#243A69'} >
+
+              <Image src={menuHamburguer} alt='menuHamburguer' style={{ maxWidth: '100%', height: 'auto' }}/>  
+            </MenuButton>
+            <MenuList  >
+              <MenuItem>Download</MenuItem>
+              <MenuItem>Create a Copy</MenuItem>
+              <MenuItem>Mark as Draft</MenuItem>
+              <MenuItem>Delete</MenuItem>
+              <MenuItem>Attend a Workshop</MenuItem>
+            </MenuList>
+          </Menu>
+          </Box>
+      </Box>
+    </Flex>
   );
 };
 
-export default Home;
+export default Header;
