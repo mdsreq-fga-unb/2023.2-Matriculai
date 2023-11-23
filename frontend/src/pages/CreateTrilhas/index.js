9a41b9fbdd3b2148803c488b5722340ddf6f518e
Yan Luca <108501120+lhuck4z@users.noreply.github.com>
2023-11-23 02:39:01 -0300

    Update index.js

--- frontend/src/pages/CreateTrilhas/index.js
+++ frontend/src/pages/CreateTrilhas/index.js
@@ -1,88 +1,99 @@
-import React, {useState} from "react";
-import { Link } from "react-router-dom";
+import React from "react";
+import { useFormik } from "formik";
 import { useNavigate } from "react-router-dom";
-import { ChakraProvider } from '@chakra-ui/react'
-import ButtonCadastrar from "../../components/Button/index.js";
+import { ChakraProvider } from "@chakra-ui/react";
+import ButtonCadastrar from "../../components/Button";
 import * as C from "./styles";
 import axios from "axios";
 import useAuth from "../../hooks/useAuth";
-import Header from "../../components/Header/index.js";
-import Footer from "../../components/Footer/index.js";
-
-
-import { 
+import Header from "../../components/Header";
+import Footer from "../../components/Footer";
+import {
   Input,
-  Button,
   Box,
   Center,
-  Flex,
   FormControl,
   FormLabel,
-  Heading,
   Select,
   Text,
   Stack,
-  Alert,
-  AlertIcon,
-  useToast,
-  Container,
-  
+  Flex,
+  Container, 
 } from "@chakra-ui/react";
-
+import { useToast } from "@chakra-ui/react";
+import * as yup from "yup";
 
 const CreateTrilhas = () => {
   const { createEletivas } = useAuth();
   const navigate = useNavigate();
-  const toastIdRef = React.useRef()
+  const toast = useToast();
 
-  const toast = useToast();  
-  const [nomeTrilha, setTrilha] = useState("");
-  const [descricao, setDescricao] = useState("");
-  const [serie, setSerie] = useState("");
-  const [eletivas, setEletivas] = useState("");
-  const [error, setError] = useState("");
+  const formik = useFormik({
+    initialValues: {
+      nomeTrilha: "",
+      descricao: "",
+      serie: "",
+      eletivas: "",
+    },
+    validationSchema: yup.object({
+      nomeTrilha: yup
+        .string()
+        .required("O nome da trilha é obrigatório")
+        .min(3, "O nome da trilha deve ter pelo menos 3 caracteres")
+        .max(40, "O nome da trilha deve ter no máximo 40 caracteres"),
+      descricao: yup
+        .string()
+        .required("A breve descrição é obrigatória")
+        .min(10, "A breve descrição deve ter pelo menos 10 caracteres")
+        .max(150, "A breve descrição deve ter no máximo 150 caracteres"),
+      serie: yup.string().required("A série é obrigatória"),
+      eletivas: yup.string().required("É necessário selecionar eletivas relacionadas"),
+    }),
+    onSubmit: async (values) => {
+      try {
+        const response = await axios.post("http://localhost:3001/learningpath/createLearningPaths", {
+          name: values.nomeTrilha,
+          description: values.descricao,
+          school_year: parseInt(values.serie),
+          electives: values.eletivas,
+        });
 
-  const handleCadastro = async () => {
-    if (!nomeTrilha || !descricao || !serie || !eletivas) {
-      
-      setError("Preencha todos os campos");
-       return;
-    };
+        if (response.status === 201) {
+          toast({
+            title: "Trilha cadastrada.",
+            description: "Trilha cadastrada com sucesso!",
+            status: "success",
+            duration: 2800,
+            isClosable: true,
+            position: "top",
+          });
 
-    try {
-      const response = await axios.post('http://localhost:3001/learningpath/createLearningPaths', {name: nomeTrilha, description: descricao, school_year: parseInt(serie), electives: eletivas });
-      
-      console.log(response)
-      if (response.status === 201) {
-        
+          // Sucesso, redirecionar ou realizar outras ações necessárias
+          navigate("/home");
+        } else {
+          // Exibir mensagem de erro
+          toast({
+            title: "Erro ao cadastrar trilha.",
+            description: response.data.message || "Erro desconhecido.",
+            status: "error",
+            duration: 2800,
+            isClosable: true,
+            position: "top",
+          });
+        }
+      } catch (error) {
+        console.error("Erro ao cadastrar:", error);
         toast({
-          title: 'Trilha cadastrada.',
-          description: "Trilha cadastrada com sucesso!",
-          status: 'success',
+          title: "Erro ao cadastrar trilha.",
+          description: "Tente novamente mais tarde.",
+          status: "error",
           duration: 2800,
           isClosable: true,
-          position: 'top'
-        })
-
-        // Sucesso, redirecionar ou realizar outras ações necessárias
-        navigate("/home");
-      } else {
-        // Exibir mensagem de erro
-        setError(response.data.message);
+          position: "top",
+        });
       }
-    } catch (error) {
-      if (axios.isAxiosError(error)) {
-        // Imprime informações detalhadas sobre o erro Axios
-        console.error("Erro ao fazer cadastro - Status:", error.response?.status);
-        console.error("Erro ao fazer cadastro - Data:", error.response?.data);
-      } else {
-        console.error("Erro ao cadastrar:", error);
-      }
-      
-      setError("Erro ao cadastrar. Tente novamente mais tarde.");
-    }
-
-  }
+    },
+  });
   return (
     <ChakraProvider>
         <Flex direction="column" minH="100vh">
@@ -108,18 +119,26 @@ const CreateTrilhas = () => {
                 size='lg'
                 isRequired
                 placeholder='Nome da trilha'
-                value={nomeTrilha}
-                onChange={(e)=>[setTrilha(e.target.value), setError("")]}
+                {...formik.getFieldProps("nomeTrilha")}
                 />
+                {formik.touched.nomeTrilha && formik.errors.nomeTrilha && (
+                  <Text color="red.500" fontSize="sm">
+                    {formik.errors.nomeTrilha}
+                  </Text>
+                )}
 
               <FormLabel color= '#243A69'>Descrição da trilha</FormLabel> 
               <Input 
                 type='text'
                 isRequired
                 placeholder='Descrição da trilha'
-                value={descricao}
-                onChange={(e)=>[setDescricao(e.target.value), setError("")]}
+                {...formik.getFieldProps("descricao")}
                 />
+                {formik.touched.descricao && formik.errors.descricao && (
+                  <Text color="red.500" fontSize="sm">
+                    {formik.errors.descricao}
+                  </Text>
+                )}
 
               <FormLabel color= '#243A69'>Série</FormLabel> 
               <Select
@@ -127,13 +146,17 @@ const CreateTrilhas = () => {
                 placeholder='Selecione a série' 
                 _placeholder={{opacity:1, color: '#243A69' }} 
                 isRequired
-                value={serie}
-                onChange={(e)=>[setSerie(e.target.value), setError("")]}
+                {...formik.getFieldProps("serie")}
                 >
                 <option value={1}> 1</option>
                 <option value={2}> 2</option>
                 <option value={3}> 3</option>
               </Select>
+              {formik.touched.serie && formik.errors.serie && (
+                  <Text color="red.500" fontSize="sm">
+                    {formik.errors.serie}
+                  </Text>
+                )}
 
               <FormLabel color= '#243A69'>Eletivas relacionadas </FormLabel>
               <Select
@@ -141,18 +164,22 @@ const CreateTrilhas = () => {
                 placeholder='Selecione as eletivas relacionadas' 
                 _placeholder={{opacity:1, color: '#243A69' }} 
                 isRequired
-                value={eletivas}
-                onChange={(e)=>[setEletivas(e.target.value), setError("")]}
+                {...formik.getFieldProps("eletivas")}
                 >
                 <option value='option1'> 1</option>
                 <option value='option2'> 2</option>
                 <option value='option3'> 3</option>
               </Select>
+              {formik.touched.eletivas && formik.errors.eletivas && (
+                  <Text color="red.500" fontSize="sm">
+                    {formik.errors.eletivas}
+                  </Text>
+                )}
               
-              <C.labelError>{error}</C.labelError>   
+              <C.labelError>{formik.errors.message}</C.labelError>
               <Center paddingBottom={5}>
 
-              <ButtonCadastrar Text="Cadastrar" onClick={handleCadastro}> </ButtonCadastrar>
+              <ButtonCadastrar Text="Cadastrar" onClick={formik.handleSubmit}></ButtonCadastrar>
               </Center>
               </Stack>
               
