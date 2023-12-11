adb8b7c2ff360f789c89d484c49ba1cd9e20d367
Yasmim Rosa <mimsayrosa@gmail.com>
2023-12-11 10:41:49 -0300

    backend abertura de matrícula

--- backend/controllers/SuperuserController.js
+++ backend/controllers/SuperuserController.js
@@ -1,3 +1,18 @@
+const Register = require('../models/schemas/RegistrationPeriod')
+
 exports.openRegistrationPeriod = async (req, res) => {
-    
+    const { name, isOpen, start, end } = req.body;
+ 
+    await Register.create({
+        name: name,
+        isOpen: isOpen,
+        start: start,
+        end: end,
+    }).then(() => {
+        res.status(201).json("OK")
+    }).catch((err) => {
+        if(err){
+            res.status(400).json({error: err});
+        };
+    });
 }
